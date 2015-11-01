701c73b757800fcee7c8e2d74a3b4c75532e2777
TEKREVI <tek.86@mail.ru>
2015-11-01 15:42:56 +0200

    Create tekAntiPlagiat.dpr

---
+++ tekAntiPlagiat.dpr
@@ -0,0 +1,17 @@
+unit tekAntiPlagiat;
+
+interface
+        {
+type
+  TMyPlagiatClass = class
+    constructor Create;
+    destructor Destroy; override;
+  private
+
+  public
+    function
+  end;
+       }
+implementation
+
+end.
